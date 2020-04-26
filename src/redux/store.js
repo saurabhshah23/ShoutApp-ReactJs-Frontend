ffb985d2b1b31da0f37855abfcbdf9e01f994a2a
Saurabh Shah <catchsau4u@gmail.com>
2020-04-26 22:20:01 +0530

    2 changes: 1. added firebase hosting init 2. changes for production issue with redux devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -14,13 +14,16 @@ const reducers = combineReducers({
 
 const initialState = {};
 
+const devTools =
+  process.env.NODE_ENV !== "production" && window.__REDUX_DEVTOOLS_EXTENSION__
+    ? window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__()
+    : (a) => a;
+console.log("process.env.NODE_ENV===", process.env.NODE_ENV);
 const store = createStore(
   reducers,
   initialState,
-  compose(
-    applyMiddleware(...middlewares),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  compose(applyMiddleware(...middlewares), devTools)
 );
 
 export default store;
